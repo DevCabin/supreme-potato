b7591fed7796c069470e740fc06b9c90803889a1
George Featherstone <DevCabin@users.noreply.github.com>
2023-11-09 11:33:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function boxTops(){
-    console.log('hello there!');
+    console.log('hello there! Let us check if we are on the same page...');
+    if (window.location.href.indexOf("f75540b7") != -1) {
+        console.log('yes, we are! Welcome');
+    } else {
+        console.log('Nope. Carry on.');
+    };
+    console.log('check complete.')
+
 }
 window.onload = boxTops();
